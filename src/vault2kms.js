d8fc24c3a469f8c6c91060fd16b8467d3af93cb7
Pierre Meunier <pierre.meunier@wealthwizards.com>
2019-12-02 12:17:27 +0000

    add: vault2kms helper
    retrieves Vault path from Consul, gets secret value
    from Vault and encrypts it with KMS

---
+++ src/vault2kms.js
@@ -0,0 +1,45 @@
+const consul = require('./consul');
+const request = require('request-promise-native');
+
+module.exports = async (path, vaultPrefix, kms, kmsKeyId) => {
+
+    if (!process.env.VAULT_TOKEN) {
+        throw new Error('Missing vault token for authentication, you need to set VAULT_TOKEN as a environment variable');
+    }
+
+    const secretPath = await consul.get(path);
+
+    const secretValue = await getSecretFromVault(secretPath, vaultPrefix);
+
+    return kmsEncrypt({
+        KeyId: kmsKeyId,
+        Plaintext: secretValue,
+    }, kms);
+}
+
+const getSecretFromVault = async (secretPath, vaultPrefix) => {
+    const vaultResponse = await request({
+        method: 'GET',
+        url: `${vaultPrefix}${secretPath}`,
+        headers: {
+          'X-Vault-Token': process.env.VAULT_TOKEN,
+        },
+        json: true,
+    });
+
+    if (vaultResponse && vaultResponse.data && vaultResponse.data.value) {
+        return vaultResponse.data.value;
+    }
+
+    throw new Error(`Missing secret at ${secretPath}`);
+}
+
+const kmsEncrypt = async (params, kms) => {
+    const data = await kms.encrypt(params).promise();
+
+    if (data && data.CiphertextBlob) {
+        return data.CiphertextBlob.toString('base64');
+    }
+
+    throw new Error('Missing encrypted secret value from AWS response');
+}
---
+++ src/vault2kms.test.js
@@ -0,0 +1,142 @@
+const test = require('tape');
+const sinon = require('sinon');
+const consul = require('./consul');
+const proxyquire = require('proxyquire').noCallThru();
+const { KMS } = require('aws-sdk');
+const kms = new KMS();
+
+const consulStub = sinon.stub(consul, 'get');
+const requestStub = sinon.stub();
+const kmsStub = sinon.stub(kms, 'encrypt');
+
+const vault2kms = proxyquire('./vault2kms', { 'request-promise-native': requestStub });
+
+test('before - fake vault token', t => {
+    process.env.VAULT_TOKEN = 'vault_token';
+    t.end();
+})
+
+test('should retrieve secret path from Consul, secret from Vault and encrypt with KMS', async assert => {
+    assert.plan(1);
+
+    consulStub.reset();
+    requestStub.reset();
+    kmsStub.reset();
+
+    consulStub
+        .withArgs('path/to/secret')
+        .resolves('secret/path');
+
+    requestStub
+        .withArgs({
+            method: 'GET',
+            url: 'http://vault/secret/path',
+            headers: {
+              'X-Vault-Token': 'vault_token',
+            },
+            json: true,
+        })
+        .resolves({
+            data: {
+                value: 'fake_secret'
+            }
+        });
+
+    kmsStub
+        .withArgs({
+            KeyId: 'kmsKeyId',
+            Plaintext: 'fake_secret'
+        })
+        .returns({
+            promise: () => Promise.resolve({
+                CiphertextBlob: Buffer.from('encrypted:fake_secret')
+            })
+        })
+
+    const encryptedSecret = await vault2kms('path/to/secret', 'http://vault/', kms, 'kmsKeyId');
+
+    assert.equal(encryptedSecret, 'ZW5jcnlwdGVkOmZha2Vfc2VjcmV0');
+});
+
+test('should throw if no data is returned from Vault', async assert => {
+
+    consulStub.reset();
+
+    consulStub
+        .withArgs('path/to/secret')
+        .resolves('secret/path');
+
+    const expectedVaultResponses = [
+        {data: {}},
+        {},
+        null
+    ];
+
+    assert.plan(expectedVaultResponses.length);
+
+    for (const response of expectedVaultResponses) {
+
+        requestStub.reset();
+        requestStub.resolves(response);
+
+        try {
+            await vault2kms('path/to/secret', 'http://vault/', kms, 'kmsKeyId');
+        } catch (e) {
+            assert.equal(e.message, 'Missing secret at secret/path');
+        }
+    }
+})
+
+
+test('should throw if encrypted secret cannot be retrieved', async assert => {
+
+    consulStub.reset();
+    requestStub.reset();
+
+    consulStub
+        .withArgs('path/to/secret')
+        .resolves('secret/path');
+
+    requestStub
+        .resolves({
+            data: {
+                value: 'fake_secret'
+            }
+        });
+
+    const expectedKmsResponses = [
+        {data: null},
+        null
+    ];
+
+    assert.plan(expectedKmsResponses.length);
+
+    for (const response of expectedKmsResponses) {
+
+        kmsStub.reset();
+        kmsStub.returns({
+            promise: () => Promise.resolve(response)
+        });
+
+        try {
+            await vault2kms('path/to/secret', 'http://vault/', kms, 'kmsKeyId');
+        } catch (e) {
+            assert.equal(e.message, 'Missing encrypted secret value from AWS response');
+        }
+    }
+})
+
+test('after - unset fake vault token', t => {
+    delete process.env.VAULT_TOKEN;
+    t.end();
+})
+
+test('should fail if vault token not present', async assert => {
+    assert.plan(1);
+
+    try {
+        await vault2kms('path/to/secret', 'http://vault/', kms, 'kmsKeyId');
+    } catch (e) {
+        assert.equal(e.message, 'Missing vault token for authentication, you need to set VAULT_TOKEN as a environment variable')
+    }
+})
